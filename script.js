f6f38c3f80f864d3f72c8cde99f62befc2ffa64b
Aldo Esteban Bojorquez Acevedo <aldo_esteban19@hotmail.com>
2026-07-23 01:59:54 -0600

    Cambios en los archivos: index, style y scripts. se ingresaron imagenes relacionadas.

--- script.js
+++ script.js
@@ -1,58 +1,55 @@
-// 1. Seleccionamos los elementos de tu HTML usando sus clases
-const slider = document.querySelector(".img-slider");
-const imagenes = document.querySelectorAll(".img-slider img");
-const btnAnt = document.querySelector(".fa-angles-left");
-const btnSig = document.querySelector(".fa-angles-right");
+function crearCarrusel(contenedorSelector) {
+    const contenedor = document.querySelector(contenedorSelector);
+    if (!contenedor) return;
 
-let indice = 0;
-let intervalo;
+    const slider = contenedor.querySelector(".img-slider");
+    const imagenes = contenedor.querySelectorAll(".img-slider img");
+    const btnAnt = contenedor.querySelector(".fa-angles-left");
+    const btnSig = contenedor.querySelector(".fa-angles-right");
 
-// 2. Función que mueve físicamente el contenedor hacia la izquierda
-function actualizarGaleria() {
-    slider.style.transform = `translateX(${-indice * 100}%)`;
-}
+    let indice = 0;
+    let intervalo;
 
-// 3. Función para ir a la siguiente foto
-function avanzarFoto() {
-    indice++;
-    // Si llega al final de las imágenes, regresa a la primera (0)
-    if (indice >= imagenes.length) {
-        indice = 0;
+    function actualizarGaleria() {
+        slider.style.transform = `translateX(${-indice * 100}%)`;
     }
-    actualizarGaleria();
-}
 
-// 4. Función para regresar a la foto anterior
-function retrocederFoto() {
-    indice--;
-    // Si retrocede desde la primera, va a la última foto
-    if (indice < 0) {
-        indice = imagenes.length - 1;
+    function avanzarFoto() {
+        indice++;
+        if (indice >= imagenes.length) indice = 0;
+        actualizarGaleria();
     }
-    actualizarGaleria();
-}
 
-// 5. Inicia el temporizador automático (5000ms = 5 segundos)
-function iniciarAuto() {
-    intervalo = setInterval(avanzarFoto, 5000);
-}
+    function retrocederFoto() {
+        indice--;
+        if (indice < 0) indice = imagenes.length - 1;
+        actualizarGaleria();
+    }
 
-// 6. Detiene y reinicia el tiempo para que no salte de golpe al hacer clic
-function reiniciarAuto() {
-    clearInterval(intervalo);
+    function iniciarAuto() {
+        intervalo = setInterval(avanzarFoto, 5000);
+    }
+
+    function reiniciarAuto() {
+        clearInterval(intervalo);
+        iniciarAuto();
+    }
+
+    btnSig.addEventListener("click", () => {
+        avanzarFoto();
+        reiniciarAuto();
+    });
+
+    btnAnt.addEventListener("click", () => {
+        retrocederFoto();
+        reiniciarAuto();
+    });
+
     iniciarAuto();
 }
 
-// 7. Conectamos los clics de tus iconos de Font Awesome con las funciones
-btnSig.addEventListener("click", () => {
-    avanzarFoto();
-    reiniciarAuto();
-});
-
-btnAnt.addEventListener("click", () => {
-    retrocederFoto();
-    reiniciarAuto();
-});
-
-// 8. Arrancamos la animación automática al cargar la página
-iniciarAuto();
+// Ahora iniciamos los 2 carruseles por separado
+crearCarrusel(".card-slider-1");
+crearCarrusel(".card-slider-2");
+crearCarrusel(".card-slider-3");
+crearCarrusel(".card-slider-4");
